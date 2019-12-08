fda73e8278414e6b3b82e3bdaf46c74f924e8c74
Qingdong Cheng <qc@QingdongdeMacBook-Pro.local>
2019-12-08 08:34:10 -0500

    add sleep method to increase the probability of dead lock

--- src/com/qingdong/deadlocks/DeadLockWithTwoThreads.java
+++ src/com/qingdong/deadlocks/DeadLockWithTwoThreads.java
@@ -4,7 +4,6 @@ public class DeadLockWithTwoThreads {
     public static void main(String[] args) {
         StringBuffer s1 = new StringBuffer();
         StringBuffer s2 = new StringBuffer();
-
         Thread a = new Thread(new ThreadA(s1, s2));
         Thread b = new Thread(new ThreadB(s1, s2));
         a.start();
@@ -30,14 +29,14 @@ class ThreadA implements Runnable {
     @Override
     public void run() {
         synchronized (s1) {
-            for (int i = 1; i < 10; i++)
-                s1.append(i);
+            //for (int i = 1; i < 10; i++)
+            s1.append(1);
             s2.append('a');
-//            try {
-//                Thread.sleep(100);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             synchronized (s2) {
                 s1.append(2);
                 s2.append('b');
@@ -57,12 +56,12 @@ class ThreadB implements Runnable {
     @Override
     public void run() {
         synchronized (s2) {
-            for (int i = -1; i > -10; i--)
-                s1.append(i);
+            //for (int i = -1; i > -10; i--)
+            s1.append(3);
             s2.append('c');
 //            System.out.println(s1);
 //            System.out.println(s2);
-            synchronized (s2) {
+            synchronized (s1) {
                 s1.append(4);
                 s2.append('d');
             }
